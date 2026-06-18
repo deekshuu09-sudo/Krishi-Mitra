6d137dddf96af580080908d95f41e5a51024c39b
deekshuu09-sudo <deekshuu09@gmail.com>
2026-06-18 20:18:35 +0530

    Fix I18nProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { I18nProvider } from './i18n/I18nContext'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { I18nProvider } from "./i18n/I18nContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <I18nProvider>
       <App />
     </I18nProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+  );
